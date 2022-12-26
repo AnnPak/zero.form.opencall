27300ff1f5bef3aa6a3a50be6d1e45d003017ae0
Anna <anna_p@rupor.agency>
2022-12-26 17:49:14 +0300

    added put request

--- src/store/slice.js
+++ src/store/slice.js
@@ -4,63 +4,32 @@ import { request } from "../utils/request";
 
 const initialState = {
   files: {},
+  uploadStatus: {}
 };
 
-export const getFileUrls = createAsyncThunk(
+export const putFile = createAsyncThunk(
   "form/getFileUrls",
-  async (file) => {
+  async ({file, inputName}) => {
       const response = await request(
           `${API_URL}/platform-api/v1/submission-proxy/pre-signed-url/?filename=${file.name}`,
           null,
           "GET"
       )
-
-      return await fetch(response.pre_signed_url, {
+      
+    
+      await fetch(response.pre_signed_url, {
         method: "PUT",
-        body: file["file"],
-      }).then(res => console.log(res));
+        body: file,
+      }).then(res=> console.log(res));
+
+      return {
+        pre_signed_url: response.pre_signed_url,
+        file_future_url: response.file_future_url,
+        filedType: inputName,
+      };
   
   }
 );
-
-// export const putFile = createAsyncThunk(
-//   "form/putFile",
-//   async ({ res, fileObject }) => {
-//     // console.log(fileObject)
-
-//     console.log(JSON.stringify(fileObject))
-
-//     const result = await fetch(res.payload.pre_signed_url, {
-//       method: "PUT",
-//       body: fileObject,
-//     });
-
-//     console.log(result)
-//   }
-// );
-
-// export const getFileUrls2 = createAsyncThunk(
-//   "form/getFileUrls",
-//   async ({ fileName, inputName }) => {
-//     if (fileName) {
-//       const response = await request(
-//         `${API_URL}/platform-api/v1/submission-proxy/pre-signed-url/?filename=${fileName}`,
-//         null,
-//         "GET"
-//       );
-
-//       return {
-//         pre_signed_url: response.pre_signed_url,
-//         file_future_url: response.file_future_url,
-//         filedType: inputName,
-//       };
-//     } else {
-//       throw new Error(`This file is undefined`);
-//     }
-//   }
-// );
-
-
 
 const formSlice = createSlice({
   name: "RootReducer",
@@ -68,27 +37,65 @@ const formSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(getFileUrls.fulfilled, (state, action) => {
-        // const { file_future_url, filedType } = action.payload;
-        // state.files = state.files
-        //   ? {
-        //       ...state.files,
-        //       [filedType]: file_future_url,
-        //     }
-        //   : { [filedType]: file_future_url };
-        console.log(action);
+      .addCase(putFile.pending, (state, action) => {
+        const inputName = action.meta.arg.inputName;
+        state.uploadStatus = state.uploadStatus
+        ? {
+            ...state.uploadStatus,
+            [inputName]: 'loading',
+          }
+        : { [inputName]: 'loading' };
+
+      })
+      .addCase(putFile.fulfilled, (state, action) => {
+        const { file_future_url, filedType } = action.payload;
+        state.files = state.files
+          ? {
+              ...state.files,
+              [filedType]: file_future_url,
+            }
+          : { [filedType]: file_future_url };
+
+          state.uploadStatus = state.uploadStatus
+          ? {
+              ...state.uploadStatus,
+              [filedType]: 'success',
+            }
+          : { [filedType]: 'success' };
+
+          const timeOut = setTimeout(() => {
+            state.uploadStatus = state.uploadStatus
+            ? {
+                ...state.uploadStatus,
+                [filedType]: 'idle',
+              }
+            : { [filedType]: 'idle' };
+          }, 5000)
+
+          clearTimeout(timeOut);
       })
 
-      .addCase(getFileUrls.rejected, (state, action) => {
-        console.log(action);
-      })
-      // .addCase(putFile.fulfilled, (state, action) => {
-      //   console.log(action);
-      // })
+      .addCase(putFile.rejected, (state, action) => {
+        const { filedType } = action.payload;
+        state.uploadStatus = state.uploadStatus
+        ? {
+            ...state.uploadStatus,
+            [filedType]: 'error',
+          }
+        : { [filedType]: 'error' };
 
-      // .addCase(putFile.rejected, (state, action) => {
-      //   console.log(action);
-      // });
+        const timeOut = setTimeout(() => {
+          state.uploadStatus = state.uploadStatus
+          ? {
+              ...state.uploadStatus,
+              [filedType]: 'idle',
+            }
+          : { [filedType]: 'idle' };
+        }, 5000)
+        
+        clearTimeout(timeOut);
+      })
+
   },
 });
 
